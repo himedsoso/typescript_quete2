0be2ed574f0979f8d99e2c9948f4da378d04dc70
himedsoso <117726033+himedsoso@users.noreply.github.com>
2023-01-04 15:39:19 +0100

    Update challenge.ts

--- challenge.ts
+++ challenge.ts
@@ -1,20 +1,17 @@
-const prettyPrintWilder = (users : user[]) => {
-    users.map((user) => {
-      console.log(`${user.name} is ${user.age} years old`);
-    });
-  };
-  
-  const wilders = user[] = [];
-  interface user {
-    name: string;
-    age?: number;
-    birthday? : string;
-  }
-
-  let user1 : user = { name: "Pierre", age: 23 };
-  const user2 : user = { name: "Paul", age: "10/02/1990" };
-  const user3 : user = { name: "Jacques", age: 25 };
-  wilders.push(user1);
-  wilders.push(user2);
-  wilders.push(user3);
-  prettyPrintWilder(wilders);
+const prettyPrintWilder = (users: User[]): void => {
+  users.map((user) => {
+    console.log(`${user.name} is ${user.age} years old`);
+  });
+};
+interface User{
+  name: string;
+  age: number;
+ }
+const wilders: User [] = [];
+const user1: User = { name: "Pierre", age: 23 };
+const user2: User = { name: "Paul", age: 32 };
+const user3: User = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
